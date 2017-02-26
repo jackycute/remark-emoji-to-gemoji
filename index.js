dab4a171188346c30b023f1741da811b3bd19a0e
Titus Wormer <tituswormer@gmail.com>
2017-02-26 13:01:23 +0100

    Refactor code-style

--- index.js
+++ index.js
@@ -1,68 +1,43 @@
-/**
- * @author jackycute
- * @copyright 2016 jackycute
- * @license MIT
- * @module remark:gemoji
- * @fileoverview
- *   Plug-in to transform emoji unicodes into gemoji shortcodes
- */
-
 'use strict';
-
-/* eslint-env commonjs */
-
-/*
- * Dependencies.
- */
 
 var visit = require('unist-util-visit');
 var escape = require('escape-string-regexp');
 var gemoji = require('gemoji');
 
+module.exports = emojiToGemoji;
+
 var regex = create();
 
-function create() {
-    var unicode = gemoji.unicode;
-    var sources = [];
-    var key;
-
-    for (key in unicode) {
-        sources.push(escape(key));
-    }
-
-    sources.sort(sort);
-
-    return new RegExp(sources.join('|'), 'g');
-
-    function sort(a, b) {
-        return b.length - a.length;
-    }
+function emojiToGemoji() {
+  return transformer;
 }
 
-/**
- * Transformer.
- *
- * @param {Node} tree - remark node to visit.
- */
 function transformer(tree) {
-    visit(tree, 'text', function (node) {
-        node.value = node.value.replace (regex, function (matched) {
-            return ':' + gemoji.unicode[matched].name + ':';
-        });
-    });
+  visit(tree, 'text', visitor);
 }
 
-/**
- * Attacher.
- *
- * @return {function(node)} - Transformer.
- */
-function attacher() {
-    return transformer;
+function visitor(node) {
+  node.value = node.value.replace(regex, replace);
 }
 
-/*
- * Expose.
- */
+function replace(matched) {
+  return ':' + gemoji.unicode[matched].name + ':';
+}
 
-module.exports = attacher;
+function create() {
+  var unicode = gemoji.unicode;
+  var sources = [];
+  var key;
+
+  for (key in unicode) {
+    sources.push(escape(key));
+  }
+
+  sources.sort(sort);
+
+  return new RegExp(sources.join('|'), 'g');
+
+  function sort(a, b) {
+    return b.length - a.length;
+  }
+}
--- test.js
+++ test.js
@@ -1,53 +1,33 @@
-/**
- * @author jackycute
- * @copyright 2016 jackycute
- * @license MIT
- * @module remark:gemoji
- * @fileoverview
- *   Plug-in to transform emoji unicodes into gemoji shortcodes
- */
-
 'use strict';
-
-/* eslint-env node */
-
-/*
- * Dependencies.
- */
 
 var test = require('tape');
 var remark = require('remark');
-var emojiToGemoji = require('./index.js');
-
 var gemoji = require('gemoji');
+var emojiToGemoji = require('./');
 
 var processor = remark().use(emojiToGemoji);
 
 var emojiList = [];
 var renderedEmojiList = [];
+
 Object.keys(gemoji.name).forEach(function (key) {
-    if (emojiList.length > 0) {
-        // do not test alias emojis
-        if (emojiList[emojiList.length - 1] != gemoji.name[key].emoji) {
-            emojiList.push(gemoji.name[key].emoji);
-            renderedEmojiList.push(':' + key + ':');
-        }
-    } else {
-        emojiList.push(gemoji.name[key].emoji);
-        renderedEmojiList.push(':' + key + ':');
+  if (emojiList.length > 0) {
+    // do not test alias emojis
+    if (emojiList[emojiList.length - 1] !== gemoji.name[key].emoji) {
+      emojiList.push(gemoji.name[key].emoji);
+      renderedEmojiList.push(':' + key + ':');
     }
+  } else {
+    emojiList.push(gemoji.name[key].emoji);
+    renderedEmojiList.push(':' + key + ':');
+  }
 });
 
-/*
- * Tests.
- */
-
 test('remark-emoji-to-gemoji', function (t) {
-    t.plan(2);
+  t.plan(2);
 
-    processor.process(emojiList.join('\n') + '\n', function (err, file) {
-        t.ifErr(err);
-
-        t.equal(String(file), renderedEmojiList.join('\n') + '\n');
-    });
+  processor.process(emojiList.join('\n') + '\n', function (err, file) {
+    t.ifErr(err);
+    t.equal(String(file), renderedEmojiList.join('\n') + '\n');
+  });
 });
